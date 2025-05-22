a64e0087c9ed5eac681a87f835ba91d377069441
TheMousavi <mousavi.amir1616@gmail.com>
2025-05-22 13:30:53 +0330

    Section RegExption

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
+const helpRegex = /please help/;
 
 const isSpam = (msg) =>false;
 
@@ -10,4 +11,9 @@ checkMessageButton.addEventListener("click", () => {
     alert("Please enter a message.");
     return;
   }
+
+    result.textContent = isSpam(messageInput.value)
+        ? "Oh no! This looks like a spam message."
+        : "This message does not seem to contain any spam.";
+    messageInput.value = "";
 });
